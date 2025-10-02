d5240169763dbba0a615e20c3904384f8a8d82c6
Dejan Gogov <denigogov@gmail.com>
2025-10-02 14:43:14 +0200

    test submodule is it working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
-import "@/test.scss";
+import "./test.scss";
+import Button from "./whitelabel/src/atoms/button/A-Button";
+import type { ButtonTypes } from "./whitelabel/src/atoms/button/a-button.types";
+
 const App = () => {
-  return <div className="test">App Component</div>;
+  const button: ButtonTypes = {
+    label: "some text",
+    size: "large",
+    type: "button",
+    style: "primary",
+    className: "test",
+  };
+
+  return (
+    <div className="test">
+      app
+      <Button {...button} />
+    </div>
+  );
 };
 
 export default App;
