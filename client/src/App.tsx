0b010466c668f783d452067eb3c6773436db5192
Dejan Gogov <denigogov@gmail.com>
2025-10-02 21:30:21 +0200

    test submodule files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,34 @@
-import "./test.scss";
-import Button from "./whitelabel/src/atoms/button/A-Button";
+import Button from "./whitelabel/src/atoms/button/a-button";
+
 import type { ButtonTypes } from "./whitelabel/src/atoms/button/a-button.types";
+import type { InputTypes } from "./whitelabel/src/atoms/input/a-button.types";
+import Input from "./whitelabel/src/atoms/input/a-input";
 
 const App = () => {
   const button: ButtonTypes = {
     label: "some text",
     size: "large",
     type: "button",
-    style: "primary",
-    className: "test",
+    style: "tertiary",
+    disabled: true,
+  };
+
+  const input: InputTypes = {
+    type: "text",
+    label: "hoho",
+    placeholder: "vmvmmv",
+    icon: {
+      name: "plus",
+      position: "left",
+    },
   };
 
   return (
     <div className="test">
       app
       <Button {...button} />
+      <h3>input</h3>
+      <Input {...input} />
     </div>
   );
 };
