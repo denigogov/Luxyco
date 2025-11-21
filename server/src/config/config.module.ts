2dbf4384e998fb3921c5c4e07fc0efbbc25a005e
Dejan Gogov <denigogov@gmail.com>
2025-11-21 01:20:45 +0100

    test commit

--- server/src/config/config.module.ts
+++ server/src/config/config.module.ts
@@ -6,7 +6,7 @@ import { envValidationSchema } from './env.validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // ConfigService available everywhere
+      isGlobal: true, // ConfigService available everywhere 123
       load: [configuration], // configuration.ts
       validationSchema: envValidationSchema,
     }),
--- server/src/modules/customers/customers.controller.ts
+++ server/src/modules/customers/customers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
@@ -22,8 +30,12 @@ export class CustomersController {
     return this.customersService.findOne(+id);
   }
 
+  //
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
     return this.customersService.update(+id, updateCustomerDto);
   }
 
