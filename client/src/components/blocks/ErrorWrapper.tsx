9676495a1cd8f50f01544324523cdd2d89ad53f4
Dejan Gogov <denigogov@gmail.com>
2026-08-23 20:52:47 +0200

    error wrapper status updated

--- client/src/components/blocks/ErrorWrapper.tsx
+++ client/src/components/blocks/ErrorWrapper.tsx
@@ -1,10 +1,15 @@
 import { getLastValidRoute } from "../../utils/routes/routeStore";
 import B_Error from "../../whitelabel/src/blocks/b-error/b-error";
 import { b_errorData } from "../../whitelabel/src/blocks/b-error/b-error.data";
+import type { ErrorTypes } from "../../whitelabel/src/blocks/b-error/b-error.types";
 
-const ErrorWrapper: React.FC = () => {
+interface ErrorWrapperType extends Partial<ErrorTypes> {}
+
+const ErrorWrapper: React.FC<ErrorWrapperType> = ({ status }) => {
   const lastVisitPage = getLastValidRoute();
-  return <B_Error lastVisitPage={lastVisitPage} {...b_errorData} />;
+  return (
+    <B_Error lastVisitPage={lastVisitPage} {...b_errorData} status={status} />
+  );
 };
 
 export default ErrorWrapper;
