0f1733cce978e70072b3b3fd34357527200c9db4
Dejan Gogov <denigogov@gmail.com>
2026-09-22 14:27:27 +0200

    prepare for deploy

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -4,15 +4,16 @@ export default () => ({
     name: 'luxyco-api',
     env: process.env.NODE_ENV || 'development',
     port: parseInt(process.env.PORT || '4000', 10),
+    frontendUrl: process.env.FRONTEND_URL,
   },
   database: {
     url: process.env.DATABASE_URL || '',
   },
   auth: {
-    jwtSecret: process.env.JWT_ACCESS_SECRET || 'dev-secret',
+    jwtSecret: process.env.JWT_ACCESS_SECRET,
     jwtExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '1d',
 
-    jwtRefreshToken: process.env.JWT_REFRESH_SECRET || 'dev-super-secret',
+    jwtRefreshToken: process.env.JWT_REFRESH_SECRET,
     jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '1d',
   },
 
